1bd17da6b88efcf8bec722e1570ab1f2edcb8641
Paul <peiche@gmail.com>
2016-08-26 10:18:02 -0500

    Updated gulpfile to include added dependency, config, and updated tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,65 @@
-var gulp     = require('gulp'),
-    inject   = require('gulp-inject'),
-    favicons = require('gulp-favicons'),
-    del      = require('del');
+var del       = require('del'),
+    favicons  = require('gulp-favicons'),
+    gulp      = require('gulp'),
+    gutil     = require('gulp-util'),
+    inject    = require('gulp-inject');
+
+var config = {
+  appName: 'gulp-favicon-inject',
+  appDescription: 'Sample Gulp project that generates a set of favicon files and injects them into the <head> of a page.',
+  url: 'https://github.com/peiche/gulp-favicon-inject',
+  version: 1.0,
+  developerName: 'Paul Eiche',
+  developerURL: 'http://eichefam.net',
+  background: '#fff',
+  path: 'dist'
+};
 
 /**
- * Remove the generated favicon.html file.
+ * Remove the generated favicon files.
  */
-gulp.task('clean-favicon', ['inject-favicon'], function() {
-  return del(['favicon.html']);
+gulp.task('clean', function() {
+  return del([config.path]);
 });
 
 /**
  * Generates favicon files used by various browsers and OS's.
+ * Requires the `clean` task to run first.
  * For creating a Material Design icon, I recommend using
  * https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html
  */
-gulp.task('favicon-generate', function() {
-  return gulp.src('src/favicon/favicon.png').pipe(favicons({
+gulp.task('favicon-generate', ['clean'], function() {
+  return gulp.src('src/favicon.png').pipe(favicons({
     appName: config.appName,
     appDescription: config.appDescription,
+    url: config.url,
+    version: config.version,
     developerName: config.developerName,
     developerURL: config.developerURL,
     background: config.background,
-    url: config.url,
-    version: config.version,
-    path: '/assets/favicon',
+    path: '/' + config.path,
     display: 'standalone',
     orientation: 'portrait',
     start_url: '/?homescreen=1',
     logging: false,
     online: false,
-    html: '../../favicon.html',
+    html: '../favicon.html',
     pipeHTML: true,
     replace: true
   }))
   .on('error', gutil.log)
-  .pipe(gulp.dest('assets/favicon'));
+  .pipe(gulp.dest(config.path));
 });
 
 /**
- * Inject the generated `favicon.html` into `default.hbs`.
- * Requires the favicon-generate task to run first.
+ * Inject the generated `favicon.html` into `index.html`.
+ * Requires the `favicon-generate` task to run first.
  */
 gulp.task('inject-favicon', ['favicon-generate'], function() {
-  gulp.src('./default.hbs')
+  gulp.src('./index.html')
   .pipe(inject(gulp.src(['./favicon.html']), {
     starttag: '<!-- inject:head:{{ext}} -->',
-    transform: function (filePath, file) {
+    transform: function(filePath, file) {
       return file.contents.toString('utf8'); // return file contents as string
     }
   }))
@@ -54,10 +67,16 @@ gulp.task('inject-favicon', ['favicon-generate'], function() {
 });
 
 /**
- * The favicon task is outside the normal build process, because ideally it
- * would only be run once, when the initial favicon graphic is done.
- * The task will generate all favicon files, including graphics and configs.
- * It will also built the asset link and meta tags in `favicon.html`, inject
- * them into `default.hbs`, and remove that generated file.
+ * Remove the generated favicon.html file.
+ * Requires the `inject-favicon` task to run first.
+ */
+gulp.task('clean-favicon', ['inject-favicon'], function() {
+  return del(['favicon.html']);
+});
+
+/**
+ * The default task will generate all favicon files, including graphics and
+ * configs. It will also build the asset link and meta tags in `favicon.html`,
+ * inject them into `index.html`, and remove the generated file.
  */
 gulp.task('default', ['clean-favicon']);
